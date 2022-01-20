efe441ed248fdde7cec3810fd5e69268c6918b57
OmarFiz <98096322+OmarFiz@users.noreply.github.com>
2022-01-20 17:56:22 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -18,8 +18,7 @@ querySnapshot.forEach((doc) => {
 async getMarkers() {
   const markers = [];
    //firebase.firestore().collection('db').get()
-   db.get()
-    .then(querySnapshot => {
+   db.get().then(querySnapshot => {
       querySnapshot.docs.forEach(doc => {
       markers.push(doc.data());
     });
