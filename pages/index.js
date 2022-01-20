0910c0ccc8d2b44795e260cd9bc077b4d0207cfd
OmarFiz <98096322+OmarFiz@users.noreply.github.com>
2022-01-20 17:51:47 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { storage } from "../firebase/config";
+import { db } from "../firebase/config";
 //import { getStorage, ref, listAll } from "firebase/storage";
 
 
