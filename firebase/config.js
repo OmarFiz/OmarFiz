63695abbfc08ac8ef8e35aeabfd29278151ca31a
OmarFiz <98096322+OmarFiz@users.noreply.github.com>
2022-01-20 13:46:58 +0100

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -4,19 +4,24 @@ import "firebase/storage";
 import "firebase/auth";
 import React, { useEffect, useState } from "react";
 
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+const firebaseApp = initializeApp({
 
-const config = {
+//const config = {
   apiKey: 'AIzaSyC_JwpXS4uj9sRRDrbFAtalE1QulNTmKnw',
   authDomain: 'visualizer-new-devs-test.firebaseapp.com',
   projectId: 'visualizer-new-devs-test',
   storageBucket: 'visualizer-new-devs-test.appspot.com',
   messagingSenderId: '702664185241',
   appId: '1:702664185241:web:580752c50d570d0c89ef08'
-}
+//}
+});
 
+const db = getFirestore();
 //export default config
 
-if (firebase.apps.length === 0) {
+/*if (firebase.apps.length === 0) {
   firebase.initializeApp(config);
 }
 
@@ -24,4 +29,4 @@ const auth = firebase.auth();
 const db = firebase.firestore();
 const storage = firebase.storage();
 
-export { db, storage, auth };
+export { db, storage, auth };*/
