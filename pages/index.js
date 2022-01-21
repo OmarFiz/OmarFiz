e7634b5444c0f42accd0c0b5ed254e85ad65b566
OmarFiz <98096322+OmarFiz@users.noreply.github.com>
2022-01-21 11:56:12 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -121,6 +121,8 @@ useEffect(() => {
 
 export default function Home () {
   return (
+    <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+
     <div className='flex flex-col items-center justify-center min-h-screen py-2'>
       <h1>ESTUDIO CACTUS VISUALIZER TEST</h1>
        <Image class="w-16 md:w-32 lg:w-48"
@@ -131,6 +133,7 @@ export default function Home () {
         alt="visualizer_room"
         />
       </div>
+     </div>
   )
 }
 
