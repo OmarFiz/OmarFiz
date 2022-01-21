3c959d073b072b7a7333ac04ec769910b2c41820
OmarFiz <98096322+OmarFiz@users.noreply.github.com>
2022-01-21 11:57:21 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,6 @@ import Image from "next/image";
 
 export default function Home () {
   return (
-    <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-
     <div className='flex flex-col items-center justify-center min-h-screen py-2'>
       <h1>ESTUDIO CACTUS VISUALIZER TEST</h1>
        <Image class="w-16 md:w-32 lg:w-48"
@@ -17,7 +15,6 @@ export default function Home () {
         alt="visualizer_room"
         />
       </div>
-     </div>
   )
 }
 
