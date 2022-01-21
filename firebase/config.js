1ccdff721a2f48661d2f85fd9a6267d64fbfcb01
OmarFiz <98096322+OmarFiz@users.noreply.github.com>
2022-01-21 10:42:55 +0100

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -50,12 +50,12 @@ poin
     // [ { id: 'glMeZvPpTN1Ah31sKcnj', title: 'The Great Gatsby' } ]
   });
 
-const mater = firebase
+const poinpoin = firebase
   .firestore()
   .collection("points")
   .doc("R4eHqq0Zi1Hb6GrP8JuV");
 
-mater.get().then((doc) => {
+poinpoin.get().then((doc) => {
  // if (!doc.exists) return;
   console.log("POINT POINT R4eHqq0Zi1Hb6GrP8JuV:", doc.data());
   // Document data: { title: 'The Great Gatsby' }
