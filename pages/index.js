3468da75860f9673b444e1722491d8e1660778df
OmarFiz <98096322+OmarFiz@users.noreply.github.com>
2022-01-21 11:53:37 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -123,12 +123,12 @@ export default function Home () {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen py-2'>
       <h1>ESTUDIO CACTUS VISUALIZER TEST</h1>
-       <Image
+       <Image class="w-16 md:w-32 lg:w-48"
         src="/imagenes/test_room.png"
-        layout="fill" 
-    width={620}
-      height={442}
-        alt="test_room"
+      //  layout="fill" 
+    //width={620}
+      //height={442}
+        alt="visualizer_room"
         />
       </div>
   )
