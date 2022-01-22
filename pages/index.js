9764fc0119682cadae03e6aa2fa8a71dc3374e4b
OmarFiz <98096322+OmarFiz@users.noreply.github.com>
2022-01-22 19:32:18 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home () {
 
   return (
        
-<div class="ng-base-pict min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12"">
+<div class="ng-base-pict min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
   <div class="relative py-3 sm:max-w-xl sm:mx-auto">
     <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20 bg-clip-padding bg-opacity-60 border border-gray-200" style="backdrop-filter: blur(20px);">
       <div class="max-w-md mx-auto">
