fd2575c0247796fe44cb31899d29d1c35cab3990
OmarFiz <98096322+OmarFiz@users.noreply.github.com>
2022-01-22 19:30:39 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,17 @@ export default function Home () {
     } 
 
   return (
-    <div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center" >
+       
+<div class="ng-base-pict min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12"">
+  <div class="relative py-3 sm:max-w-xl sm:mx-auto">
+    <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20 bg-clip-padding bg-opacity-60 border border-gray-200" style="backdrop-filter: blur(20px);">
+      <div class="max-w-md mx-auto">
         <div>
-          <img src="/imagenes/fingerprint.png" />
+          <img src="/imagenes/fingerprint.png" class="h-7 sm:h-8" />
         </div>
+       </div>
+     </div>
+    </div>
 </div>
 
   )
