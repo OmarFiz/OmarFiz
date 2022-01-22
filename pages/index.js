be2a77bf5bc138c38846843344b814f539c1bee3
OmarFiz <98096322+OmarFiz@users.noreply.github.com>
2022-01-22 19:34:02 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home () {
        
 <div class="bg-base-pict min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
   <div class="relative py-3 sm:max-w-xl sm:mx-auto">
-    <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20 bg-clip-padding bg-opacity-60 border border-gray-200" style="backdrop-filter: blur(20px);">
+    <div class="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20 bg-clip-padding bg-opacity-60 border border-gray-200">
       <div class="max-w-md mx-auto">
         <div>
           <img src="/imagenes/fingerprint.png" class="h-7 sm:h-8" />
