d9fad65261611bd92ad880135edca277bbcae1f1
OmarFiz <98096322+OmarFiz@users.noreply.github.com>
2022-01-22 19:26:16 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,14 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: {
-    extend: {},
-  },
+extend: {
+  
+},
+    theme: {
+    backgroundImage: {
+      'base-pict': "url('https://firebasestorage.googleapis.com/v0/b/porcelanosa-partners-spaces.appspot.com/o/projects%2FdorptVQTHsbkYC60NSlt%2Fscenes%2F1567170849457-base?alt=media&token=cf8bcee2-bf89-4fd9-8bfd-9d4462348844')"
+      }
+    },
+
   plugins: [],
 }
