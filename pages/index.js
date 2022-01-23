48258d30b9213d5cf29b9d648e31ae6517c8e625
OmarFiz <98096322+OmarFiz@users.noreply.github.com>
2022-01-23 15:18:04 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home () {
     const showMask = () => {
     console.log('Click!!!!');
        //  document.getElementById("surprise").innerHTML="<Image src='/imagenes/test_room.png' alt='Logo' top = {10} left = {10} width={88} height={88} />";
-var node = document.getElementById('maskscrol')
+var node = document.getElementById('imageClick')
         node.style.visibility = 'visible';
 
          console.log('Claaaaack!!!!');       
@@ -21,7 +21,7 @@ var node = document.getElementById('maskscrol')
     
   return (
 <div class="container mx-auto relative bg-base-pict h-screen max-h-screen bg-cover bg-center bg-no-repeat min-h-screen py-6 flex flex-col justify-center sm:py-12">
-       <div class="w-1/4 h-1/4 absolute top-1/3 left-3/4 rounded-lg p-4"><img src="/imagenes/fingerprint.png" class="h-14 sm:h-14 hover:opacity-100" onClick={() => showMask()}"/></div>
+       <div class="w-1/4 h-1/4 absolute top-1/3 left-3/4 rounded-lg p-4"><img src="/imagenes/fingerprint.png" class="h-14 sm:h-14 hover:opacity-100" onClick={() => imageClick()}/></div>
        <div class="w-1/4 h-1/4 absolute top-3/4 left-1/4 rounded-lg p-4"><img src="/imagenes/fingerprint.png" class="h-14 sm:h-14 hover:opacity-100" /></div>
  
      <div id="maskscrol"  class="absolute invisible h-32 scroll-snap-type-y-mandatory w-full">
