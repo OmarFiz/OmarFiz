2bb6ee2aa533178b874205414790b249387debfd
OmarFiz <98096322+OmarFiz@users.noreply.github.com>
2022-01-23 19:07:03 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home () {
     console.log('Click!!!!');
         document.getElementById('maskscrol').style.visibility = 'visible';
     } 
-     const layeralmond = () => {
+   /*  const layeralmond = () => {
     console.log('layeralmond!!!!');
         document.getElementById('masksalmond').style.visibility = 'visible';
 
@@ -18,7 +18,7 @@ export default function Home () {
     console.log('layernatural!!!!');
         document.getElementById('masksnatural').style.visibility = 'visible';
 
-    } 
+    } */
      const layertaupe = () => {
     console.log('layertaupe!!!!');
         document.getElementById('maskstaupe').style.visibility = 'visible';
@@ -32,23 +32,23 @@ export default function Home () {
 
   return (
 <div class="container mx-auto relative bg-base-pict h-screen max-h-screen bg-cover bg-center bg-no-repeat min-h-screen py-6 flex flex-col justify-center sm:py-12" >
-<div id="masksalmond" class="container invisible mx-auto relative bg-layer-almond h-screen max-h-screen bg-cover bg-center bg-no-repeat min-h-screen py-6 flex flex-col justify-center sm:py-12" />
-<div id="masksnatural" class="container invisible mx-auto relative bg-layer-natural h-screen max-h-screen bg-cover bg-center bg-no-repeat min-h-screen py-6 flex flex-col justify-center sm:py-12"/>
+//<div id="masksalmond" class="container invisible mx-auto relative bg-layer-almond h-screen max-h-screen bg-cover bg-center bg-no-repeat min-h-screen py-6 flex flex-col justify-center sm:py-12" />
+//<div id="masksnatural" class="container invisible mx-auto relative bg-layer-natural h-screen max-h-screen bg-cover bg-center bg-no-repeat min-h-screen py-6 flex flex-col justify-center sm:py-12"/>
 <div id="maskstaupe" class="container invisible mx-auto relative bg-layer-taupe h-screen max-h-screen bg-cover bg-center bg-no-repeat min-h-screen py-6 flex flex-col justify-center sm:py-12"/>
        
      <div class="w-1/4 h-1/4 absolute top-1/3 left-3/4 rounded-lg p-4"><img src="/imagenes/fingerprint.png" class="h-14 sm:h-14 hover:opacity-100" onClick={() => imageClick()}/></div>
      <div class="w-1/4 h-1/4 absolute top-3/4 left-1/4 rounded-lg p-4"><img src="/imagenes/fingerprint.png" class="h-14 sm:h-14 hover:opacity-100 " onClick={() => layerreset()}/></div>
     
-     <div id="maskscrol"  class="absolute invisible h-32 scroll-snap-type-y-mandatory w-auto bg-brown-100 rounded-full border-1">
-                 <section>
+     <div id="maskscrol"  class="absolute invisible h-32 scroll-snap-type-y-mandatory w-auto">
+                 <section  class="bg-gray-100 rounded-full border-1">
                    <span class="inline-block"><img src="https://firebasestorage.googleapis.com/v0/b/porcelanosa-partners-spaces.appspot.com/o/materials%2F1563187053154%2Fpreview?alt=media&token=d5dcbb60-8267-43d6-94e6-2ab245ce6583" class=" h-20 w-20" alt="" onClick={() => layeralmond()} /></span>
                     <span > Tanzania Almond</span>
                  </section>
-                 <section>
+                 <section  class="bg-gray-100 rounded-full border-1">
                    <span class="inline-block"><img  src="https://firebasestorage.googleapis.com/v0/b/porcelanosa-partners-spaces.appspot.com/o/materials%2F1563187274393%2Fpreview?alt=media&token=1006c9f0-30ee-41e2-979e-0797df79d219" class=" h-20 w-20" alt="" onClick={() => layernatural()}/></span>
                    <span> Tanzania Natural</span>
                  </section>
-                 <section>
+                 <section  class="bg-gray-100 rounded-full border-1">
                    <span class="inline-block"><img src="https://firebasestorage.googleapis.com/v0/b/porcelanosa-partners-spaces.appspot.com/o/materials%2F1563187566938%2Fpreview?alt=media&token=ed0af5cc-aca5-4ae5-84f3-699d6d1710b4" class=" h-20 w-20" alt="" onClick={() => layertaupe()}/></span>
                    <span> Tanzania Taupe</span>
                  </section>
