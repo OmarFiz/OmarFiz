631063647dcc83a924cfbdfe976322f044bf78ae
OmarFiz <98096322+OmarFiz@users.noreply.github.com>
2022-01-24 11:31:20 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home () {
 
 
     const imageClick1 = () => {
-         document.getElementById('fingerprint1').onclick = console.log("Button clicked, id "+this.innerHTML);
+         document.getElementById('fingerprint1').onclick = console.log("Button clicked, id ");
              console.log('imageClick1!!!!');
 
         document.getElementById('maskscrol').style.visibility = 'visible';
