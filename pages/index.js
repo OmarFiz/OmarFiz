e89a2baf67abc77b4183064c53de7f7f9654a3be
OmarFiz <98096322+OmarFiz@users.noreply.github.com>
2022-01-24 11:28:42 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,10 @@ import Image from "next/image";
      
 export default function Home () {
      
-var reply_click = function()
-{
-    console.log("Button clicked, id "+this.id+", text"+this.innerHTML);
-}
+
 
     const imageClick1 = () => {
-         document.getElementById('fingerprint1').onclick = reply_click;
+         document.getElementById('fingerprint1').onclick = console.log("Button clicked, id "+this.id+", text"+this.innerHTML);
              console.log('imageClick1!!!!');
 
         document.getElementById('maskscrol').style.visibility = 'visible';
